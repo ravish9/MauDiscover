4da823219d989d3ffc43fdd74f133c23f8f678ed
Shaivin Bhekharee <bshaivin@gmail.com>
2019-11-02 08:51:14 +0400

    Hotel, Shopping, Restaurant

--- app/src/main/java/com/example/prototype1/HotelActivity.java
+++ app/src/main/java/com/example/prototype1/HotelActivity.java
@@ -2,7 +2,11 @@ package com.example.prototype1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class HotelActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class HotelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
+
+        configureMenuButton();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void configureMenuButton() {
+        ImageButton menu_button = findViewById(R.id.imageButton);
+        menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HotelActivity.this, mainMenu.class));
+            }
+        });
     }
 }

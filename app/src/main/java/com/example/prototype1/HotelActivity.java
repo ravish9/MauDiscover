03d068eb512395b174595c737d426c57caa02b7e
Shaivin Bhekharee <bshaivin@gmail.com>
2019-11-02 09:57:57 +0400

    Hotel, Shopping, Restaurant

--- app/src/main/java/com/example/prototype1/HotelActivity.java
+++ app/src/main/java/com/example/prototype1/HotelActivity.java
@@ -7,13 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.net.Uri;
 
 public class HotelActivity extends AppCompatActivity {
+
+    ImageButton androidImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotel);
+
+        
+
+        androidImageButton = (ImageButton) findViewById(R.id.image_button_android);
+
+        androidImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.angsana.com/en"));
+                startActivity(intent);
+            }
+        });
+
 
         configureMenuButton();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);

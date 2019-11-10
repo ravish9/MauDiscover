acdfba78258e29cb635c11fc9b825002b03c210d
Shaivin Bhekharee <bshaivin@gmail.com>
2019-11-10 21:04:58 +0400

    Top Picks

--- app/src/main/java/com/example/prototype1/HotelActivity.java
+++ app/src/main/java/com/example/prototype1/HotelActivity.java
@@ -8,17 +8,14 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.net.Uri;
+import android.widget.Button;
 
-
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HotelActivity extends AppCompatActivity {
 
-    ImageButton androidImageButton;
-    ImageButton androidImageButton2;
-    ImageButton androidImageButton3;
-    ImageButton androidImageButton4;
-    ImageButton androidImageButton5;
-    ImageButton androidImageButton6;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,90 +23,4 @@ public class HotelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hotel);
 
 
-
-
-        androidImageButton = (ImageButton) findViewById(R.id.image_button_android);
-        androidImageButton2 = (ImageButton) findViewById(R.id.image_button_android2);
-        androidImageButton3 = (ImageButton) findViewById(R.id.image_button_android3);
-        androidImageButton4 = (ImageButton) findViewById(R.id.image_button_android4);
-        androidImageButton5 = (ImageButton) findViewById(R.id.image_button_android5);
-        androidImageButton6 = (ImageButton) findViewById(R.id.image_button_android6);
-
-        androidImageButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://www.angsana.com/en"));
-                startActivity(intent);
-            }
-        });
-
-        androidImageButton2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://www.luxresorts.com/en/mauritius/hotel/luxbellemare/"));
-                startActivity(intent);
-            }
-        });
-
-        androidImageButton3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://www.booking.com/hotel/mu/intercontinental-mauritius.en-gb.html?aid=356980;label=gog235jc-1DCAsonQFCGmludGVyY29udGluZW50YWwtbWF1cml0aXVzSDNYA2idAYgBAZgBCbgBF8gBDNgBA-gBAYgCAagCA7gC5pD77QXAAgE;sid=a839bd61a1d9d9018730638599d07b4e;dist=0&keep_landing=1&sb_price_type=total&type=total&"));
-                startActivity(intent);
-            }
-        });
-
-        androidImageButton4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://www3.hilton.com/en/hotels/mauritius/hilton-mauritius-resort-and-spa-MRUHIHI/index.html"));
-                startActivity(intent);
-            }
-        });
-
-        androidImageButton5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://www.fourseasons.com/mauritius/"));
-                startActivity(intent);
-            }
-        });
-
-        androidImageButton6.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("http://www.blueberyl.com/"));
-                startActivity(intent);
-            }
-        });
-
-
-
-
-
-        configureMenuButton();
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
-
-    private void configureMenuButton() {
-        ImageButton menu_button = findViewById(R.id.imageButton);
-        menu_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HotelActivity.this, mainMenu.class));
-            }
-        });
-    }
-}
+}}

4ade318a6b8e37b48b2ef3b8d7122fe055816234
Shaivin Bhekharee <bshaivin@gmail.com>
2020-04-05 16:12:02 +0400

    Currency Converter with Voice

--- app/src/main/java/com/example/prototype1/VoiceActivity.java
+++ app/src/main/java/com/example/prototype1/VoiceActivity.java
@@ -32,71 +32,215 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.speech.RecognizerIntent;
 //import android.support.v4.app.NavUtils;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class VoiceActivity extends Activity  implements OnClickListener{
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-    public ListView mList;
-    public Button speakButton;
+import java.util.ArrayList;
+import java.util.Locale;
 
-    public static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+public class VoiceActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
+
+    TextView firstNumTextView;
+    //TextView secondNumTextView;
+    TextView operatorTextView;
+    TextView resultTextView;
+    Button goButton;
+
+    TextToSpeech textToSpeech;
+
+    private float FIRST_NUMBER;
+    private int SECOND_NUMBER;
+    private char OPERATOR;
+    private float RESULT;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
 
-        speakButton = (Button) findViewById(R.id.btn_speak);
-        speakButton.setOnClickListener(this);
+        textToSpeech = new TextToSpeech(this, this);
 
-        voiceinputbuttons();
-    }
+        firstNumTextView = findViewById(R.id.firstNumTextView);
+        //secondNumTextView = findViewById(R.id.secondNumTextView);
+        operatorTextView = findViewById(R.id.operatorTextView);
+        resultTextView = findViewById(R.id.resultTextView);
+        goButton = findViewById(R.id.goButton);
 
-    public void informationMenu() {
-        startActivity(new Intent("android.intent.action.INFOSCREEN"));
-    }
+        firstNumTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+                startActivityForResult(intent, 10);
+            }
+        });
 
-    public void voiceinputbuttons() {
-        speakButton = (Button) findViewById(R.id.btn_speak);
-       // mList = (ListView) findViewById(R.id.list);
-    }
+        //secondNumTextView.setOnClickListener(new View.OnClickListener() {
+        // @Override
+        // public void onClick(View view) {
+        //  Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        //  intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+        //startActivityForResult(intent, 20);
+        // }
+        // });
 
-    public void startVoiceRecognitionActivity() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                "Speech recognition demo");
-        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
-    }
+        operatorTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+                startActivityForResult(intent, 30);
+            }
+        });
 
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
-        startVoiceRecognitionActivity();
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RESULT = performCalculations();
+                resultTextView.setText(String.valueOf(RESULT));
+                textToSpeech.speak(String.valueOf(RESULT )+"Mauritian Rupees", TextToSpeech.QUEUE_ADD, null);
+            }
+        });
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
-            // Fill the list view with the strings the recognizer thought it
-            // could have heard
-            ArrayList matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            mList.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, matches));
-            // matches is the result of voice input. It is a list of what the
-            // user possibly said.
-            // Using an if statement for the keyword you want to use allows the
-            // use of any activity if keywords match
-            // it is possible to set up multiple keywords to use the same
-            // activity so more than one word will allow the user
-            // to use the activity (makes it so the user doesn't have to
-            // memorize words from a list)
-            // to use an activity from the voice input information simply use
-            // the following format;
-            // if (matches.contains("keyword here") { startActivity(new
-            // Intent("name.of.manifest.ACTIVITY")
-
-            if (matches.contains("information")) {
-                informationMenu();
+        if (resultCode == RESULT_OK && data != null) {
+            switch (requestCode) {
+                case 10:
+                    int intFound = getNumberFromResult(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS));
+                    if (intFound != -1) {
+                        FIRST_NUMBER = intFound;
+                        firstNumTextView.setText(String.valueOf(intFound));
+                    } else {
+                        //Toast.makeText(getApplicationContext(), "Sorry, I didn't catch that! Please try again", Toast.LENGTH_LONG).show();
+                        textToSpeech.speak("Sorry Number not available. Please try again ", TextToSpeech.QUEUE_ADD, null);
+                    }
+                    break;
+                //case 20:
+                //intFound = getNumberFromResult(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS));
+                // if (intFound != -1) {
+                //SECOND_NUMBER = intFound;
+                //secondNumTextView.setText(String.valueOf(intFound));
+                //} else {
+                // Toast.makeText(getApplicationContext(), "Sorry, I didn't catch that! Please try again", Toast.LENGTH_LONG).show();
+                //  }
+                // break;
+                case 30:
+                    char operatorFound = getOperatorFromResult(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS));
+                    if (operatorFound != '0') {
+                        OPERATOR = operatorFound;
+                        operatorTextView.setText(String.valueOf(operatorFound));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Sorry, I didn't catch that! Please try again", Toast.LENGTH_LONG).show();
+                    }
             }
-        }}}
+        } else {
+            Toast.makeText(getApplicationContext(), "Failed to recognize speech!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // method to loop through results trying to find a number
+    private int getNumberFromResult(ArrayList<String> results) {
+        for (String str : results) {
+            if (getIntNumberFromText(str) != -1) {
+                return getIntNumberFromText(str);
+            }
+        }
+        return -1;
+    }
+
+    // method to loop through results trying to find an operator
+    private char getOperatorFromResult(ArrayList<String> results) {
+        for (String str : results) {
+            if (getCharOperatorFromText(str) != '0') {
+                return getCharOperatorFromText(str);
+            }
+        }
+        return '0';
+    }
+
+    // method to convert string number to integer
+    private int getIntNumberFromText(String strNum) {
+        switch (strNum) {
+            case "zero":
+                return 0;
+            case "one":
+                return 1;
+            case "two":
+                return 2;
+            case "three":
+                return 3;
+            case "four":
+                return 4;
+            case "five":
+                return 5;
+            case "six":
+                return 6;
+            case "seven":
+                return 7;
+            case "eight":
+                return 8;
+            case "nine":
+                return 9;
+        }
+        return -1;
+    }
+
+    // method to convert string operator to char
+    private char getCharOperatorFromText(String strOper) {
+        switch (strOper) {
+            case "Pound":
+                return '£';
+            case "Yuan":
+            case "Yen":
+                return '¥';
+            case "Euro":
+                return '€';
+            case "Indian rupee":
+                return '₹';
+            case "Dollar":
+                return '$';
+
+
+        }
+        return '0';
+    }
+
+    private float performCalculations() {
+        switch (OPERATOR) {
+            case '₹':
+                return (float) (FIRST_NUMBER *0.53);
+            case '€':
+                return (float) (FIRST_NUMBER *42.8);
+            case '¥':
+                return (float) (FIRST_NUMBER * 37.4);
+            case '£':
+                return (float) (FIRST_NUMBER * 40);
+            case '$':
+                return (float) (FIRST_NUMBER * 35);
+
+
+        }
+        return -999;
+    }
+
+    @Override
+    public void onInit(int i) {
+
+    }
+}
